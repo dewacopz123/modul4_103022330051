fe0e29fbc027470287a218def8d2dae21ac6642b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:09:15 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022330051
+{
+    class KodeProduk
+    {
+        private static readonly Dictionary<string, string> kodeProdukMap = new Dictionary<string, string>
+        {
+            {"Laptop", "E100" },
+            {"Smartphone", "E101" },
+            {"Tablet", "E102" },
+            {"Headset", "E103" },
+            {"Keyboard", "E104" },
+            {"Mouse", "E105" },
+            {"Printer", "E106" },
+            {"Monitor", "E107" },
+            {"Smartwatch", "E108" },
+            {"Kamera", "E109" }
+        };
+
+        private static string getKodeProduk(string kodeProduk)
+        {
+            return kodeProdukMap.TryGetValue(kodeProduk, out string kodePeoduk) ? kodeProduk : "kode produk tidak ditemukan";
+        }
+    }
+}
+
